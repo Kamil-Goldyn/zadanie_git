5c1a3ab5b018de66368dc3720d9bc932f0daad0f
Kamil <kamil.goldyn0@gmail.com>
2025-06-16 17:25:45 +0200

    dodanie funkcji obliczającej silnie

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,18 +2,44 @@
 
 using namespace std;
 
+long long calculateFactorial(int n) {
+    if (n < 0) {
+        return -1;
+    }
+    long long factorial = 1;
+    for (int i = 1; i <= n; ++i) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
 int main() {
-    int a, b;
-    cout << "Podaj liczbe calkowita ";
-    cin >> a >> b;
+    int a;
+    cout << "Podaj liczbe calkowita: ";
+    cin >> a;
+
     int wyjscie;
     do {
-        cout << endl;
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+        cout << "1. Wyznacz silnie liczby" << endl;
         cin >> wyjscie;
+
+        if (wyjscie == 1) {
+            long long factorialResult = calculateFactorial(a);
+            if (factorialResult == -1) {
+                cout << "Silnia nie jest zdefiniowana dla liczb ujemnych." << endl;
+            } else {
+                cout << "Silnia liczby " << a << " wynosi: " << factorialResult << endl;
+            }
+        } else if (wyjscie != 0) {
+            cout << "Nieprawidlowy wybor. Sprobuj ponownie." << endl;
+        }
+
     } while(wyjscie != 0);
+
+    cout << "Koniec programu." << endl;
     return 0;
 }
